4d3bd14e7f1e5933564852b4dbdaaed62519c353
foobar42fsm <webelliot@fordcochrane.com>
2018-02-16 21:59:46 -0500

    jk nvrmind

--- app.js
+++ app.js
@@ -887,6 +887,6 @@ app.listen(app.get('port'), function() {
 
 module.exports = app;
 
-http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
