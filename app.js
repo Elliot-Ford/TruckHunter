76c37a2c03f5368c04a0adcd9ca54c7e190469e2
foobar42fsm <webelliot@fordcochrane.com>
2018-02-17 06:11:14 -0500

    2nd try

--- app.js
+++ app.js
@@ -94,12 +94,70 @@ function verifyRequestSignature(req, res, buf) {
  * setup is the same token used here.
  *
  */
-app.get('/webhook', function(req, res) {
-  webhook.get(req, res);
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log('Validating webhook');
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error('Failed validation. Make sure the validation tokens match.');
+    res.sendStatus(403);
+  }
 });
 
-app.post('/webhook', function(req, res) {
-  webhook.post(req, res)
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+app.post('/webhook', function (req, res) {
+ var data = req.body;
+
+ // Make sure this is a page subscription
+ if (data.object == 'page') {
+   // Iterate over each entry
+   // There may be multiple if batched
+
+   data.entry.forEach(function (pageEntry) {
+     var pageID = pageEntry.id;
+     var timeOfEvent = pageEntry.time;
+
+   // // Gets the body of the webhook event
+   // let webhook_event = entry.messaging[0];
+   // console.log(webhook_event);
+   //
+   // // Get the sender PSID
+   // let sender_psid = webhook_event.sender.id;
+   // console.log('Sender PSID: ' + sender_psid);
+
+     // Iterate over each messaging event
+     pageEntry.messaging.forEach(function (messagingEvent) {
+       if (messagingEvent.optin) {
+         webhook.receivedAuthentication(messagingEvent);
+       } else if (messagingEvent.message) {
+         webhook.receivedMessage(messagingEvent);
+       } else if (messagingEvent.delivery) {
+         webhook.receivedDeliveryConfirmation(messagingEvent);
+       } else if (messagingEvent.postback) {
+         webhook.receivedPostback(messagingEvent);
+       } else if (messagingEvent.read) {
+         webhook.receivedMessageRead(messagingEvent);
+       } else if (messagingEvent.account_linking) {
+         webhook.receivedAccountLink(messagingEvent);
+       } else {
+         console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+       }
+     });
+   });
+
+   // Assume all went well.
+   //
+   // You must send back a 200, within 20 seconds, to let us know you've
+   // successfully received the callback. Otherwise, the request will time out.
+   res.sendStatus(200);
+ }
 });
 
 /*
@@ -128,7 +186,7 @@ app.get('/authorize', function(req, res) {
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid
 // certificate authority.
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,101 +1,3 @@
-exports.get = function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log('Validating webhook');
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error('Failed validation. Make sure the validation tokens match.');
-    res.sendStatus(403);
-  }
-}
-
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same
- * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page.
- * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
- *
- */
- exports.post = function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-
-    // // Gets the body of the webhook event
-    // let webhook_event = entry.messaging[0];
-    // console.log(webhook_event);
-    //
-    // // Get the sender PSID
-    // let sender_psid = webhook_event.sender.id;
-    // console.log('Sender PSID: ' + sender_psid);
-
-
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          receivedPostback(messagingEvent);
-        } else if (messagingEvent.read) {
-          receivedMessageRead(messagingEvent);
-        } else if (messagingEvent.account_linking) {
-          receivedAccountLink(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-}
-
-
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    // For testing, let's log an error. In production, you should throw an
-    // error.
-    console.error("Couldn't validate the signature.");
-  } else {
-    var elements = signature.split('=');
-    var method = elements[0];
-    var signatureHash = elements[1];
-
-    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-                        .update(buf)
-                        .digest('hex');
-
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
-
 /*
  * Authorization Event
  *
@@ -104,7 +6,7 @@ function verifyRequestSignature(req, res, buf) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/authentication
  *
  */
-function receivedAuthentication(event) {
+exports.receivedAuthentication = function(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfAuth = event.timestamp;
@@ -139,7 +41,7 @@ function receivedAuthentication(event) {
  * then we'll simply confirm that we've received the attachment.
  *
  */
-function receivedMessage(event) {
+exports.receivedMessage = function (event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
@@ -206,7 +108,7 @@ function receivedMessage(event) {
  * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
  *
  */
-function receivedDeliveryConfirmation(event) {
+exports.receivedDeliveryConfirmation = function(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var delivery = event.delivery;
@@ -232,7 +134,7 @@ function receivedDeliveryConfirmation(event) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
  *
  */
-function receivedPostback(event) {
+exports.receivedPostback = function(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
@@ -256,7 +158,7 @@ function receivedPostback(event) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
  *
  */
-function receivedMessageRead(event) {
+exports.receivedMessageRead = function (event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
 
@@ -276,7 +178,7 @@ function receivedMessageRead(event) {
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/account-linking
  *
  */
-function receivedAccountLink(event) {
+exports.receivedAccountLink = function (event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
 
