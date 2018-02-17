d769e4f039d1f8d42087ebebfba154820dbdc293
foobar42fsm <webelliot@fordcochrane.com>
2018-02-17 07:39:07 -0500

    made the number regex just be 0-9 case

--- app.js
+++ app.js
@@ -270,7 +270,16 @@ function receivedMessage (event) {
         sendHiMessage(senderID);
         break;
 
-      case '\d':
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
         if(true) {
           sendLocationMessage(senderID);
         }
